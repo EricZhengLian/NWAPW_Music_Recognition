aef5a9ebac1c5aec997ddf9aecaf1563a3072e7e
theangrybagel <bagel@Trevors-MacBook-Pro.local>
2020-07-22 13:33:02 -0700

    Spiced up the website a bit. You can now upload files, the next step is to make those files save to a directory.

--- site/index.js
+++ site/index.js
@@ -1,6 +1,26 @@
 var express = require('express');
+const port = "3200";
+const parser = require('body-parser');
+
+
+//const { body,validationResult } = require('express-validator/check');
+//const { sanitizeBody } = require('express-validator/filter');
+
 var app = express();
 
 app.use(express.static(__dirname + '/www'));
-app.listen('3100');
-console.log('working on 3100');
+app.use(parser.urlencoded({extended: true}));
+
+app.post('/upload', (req, res) => {
+	console.log("Name: ", req.body.audio_name);
+	console.log("Artist: ", req.body.artist_name);
+	console.log("File: ", req.body.file)
+	res.redirect("/");
+});
+
+app.listen(port);
+console.log('working on port ' + port);
+/*
+body("audio_name", "Invallid Name").isLength({min: 2})
+body("artist_name", "Invallid Artist").isLength({min: 2})
+*/
