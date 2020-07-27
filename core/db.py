8987fc6072e9b8b5aebc21d09f27a289f4a20567
EricZhengLian <2892838595@qq.com>
2020-07-27 22:20:30 +0800

    core program added

---
+++ core/db.py
@@ -0,0 +1,108 @@
+import fingerprint
+import os
+import librosa
+import glob
+from hashlib import sha1
+import numpy as np
+import pandas as pd
+import scipy
+from collections import Counter
+import time
+
+class Database:
+    def __init__(self):
+        self.song_table = pd.DataFrame({'song_id':[], 'name':[]})
+        self.fingerprint_table = pd.DataFrame({'song_id':[], 'hash':[], 'offset':[]})
+    
+    def addRow(self, table, row):
+        table.loc[len(table)] = row
+       
+    def parse_file_hash(self, filename):
+        s = sha1()
+        with open(filename , "rb") as f:
+            while True:
+                buf = f.read(2**20)
+                if not buf: 
+                    break
+                s.update(buf)
+
+        return s.hexdigest().upper()
+        
+    def add(self, f): # f is a wav file
+        x, fs = librosa.load(f)
+        song_id = self.parse_file_hash(f)
+        self.addRow(self.song_table, [song_id, f])
+        hashes = set(fingerprint.fingerprint(x, fs))
+        for hash_, offset in hashes:
+            self.addRow(self.fingerprint_table, [song_id, hash_, int(offset)])
+        print(f'{f} is added')
+            
+    def get_song_by_id(self, song_id):
+        return self.song_table[self.song_table['song_id'] == song_id].values[0][1]
+    
+    def get_song_hashes_count(self, song_id):
+        return len(self.fingerprint_table.loc[self.fingerprint_table['song_id'] == song_id])
+            
+    def train(self, training_dir):
+        t0 = time.process_time()
+        i = 0
+        for f in glob.iglob(training_dir+'/*.wav'):
+            self.add(f)
+            i += 1
+        t = time.process_time()
+        print(f'training time: {t-t0}; number of files added: {i}')
+        
+    def save(self, s, f): #s, f are both csv files. 
+        self.song_table.to_csv(s)
+        self.fingerprint_table.to_csv(f)
+        
+    def load(self, s, f):
+        self.song_table = pd.read_csv(s, index_col=0)
+        self.fingerprint_table = pd.read_csv(f, index_col=0)
+        
+    def find_matches(self, f): # f is a wav file
+        x, fs = librosa.load(f)
+        hashes = set(fingerprint.fingerprint(x, fs))
+        return self.return_matches(hashes)
+    
+    def return_matches(self, hashes):
+        mapper = {}
+        for hash_, offset in hashes:
+            mapper[hash_] = offset
+        values = mapper.keys() #hashe values
+        results = []
+        for unique_value in set(values):
+            matched = self.fingerprint_table.loc[self.fingerprint_table['hash'] == unique_value]
+            if len(matched) == 0:
+                continue
+            else:
+                for id_,offset in matched[['song_id', 'offset']].values:
+                    results.append((id_, int(offset-mapper[unique_value])))
+        return results
+    
+    def align_matches(self, matches):
+        if len(matches) == 0:
+            return {}
+        cnt = Counter(matches)
+       # for match in cnt:
+           # cnt[match] /= self.get_song_hashes_count(match[0])
+        best_guess = cnt.most_common(1)[0]
+        largest_count = best_guess[1]
+        song_id = best_guess[0][0]
+        offset_difference = best_guess[0][1]
+        song_name = self.get_song_by_id(song_id)
+        nsec = round(float(largest_count)/44100*2048, 5)
+        return {
+        "SONG_ID" : song_id,
+        "SONG_NAME" : song_name,
+        "CONFIDENCE" : largest_count/len(matches),
+        "OFFSET_DIFFERENCE" : offset_difference,
+        "OFFSET_DIFFERENCE_IN_SEC": nsec
+        }
+    
+    def query(self, f):
+        t0 = time.process_time()
+        output = self.align_matches(self.find_matches(f))
+        t = time.process_time()
+        print("Query time:", t-t0)
+        return output
---
+++ core/fingerprint.py
@@ -0,0 +1,109 @@
+import hashlib
+import scipy
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.ndimage.filters import maximum_filter
+from scipy.ndimage.morphology import (binary_erosion,
+                                      generate_binary_structure,
+                                      iterate_structure)
+
+import librosa
+import librosa.display
+import IPython.display as ipd
+import pandas as pd
+from statistics import mode
+from collections import Counter
+
+def fingerprint(x, fs):
+    '''
+    Implementation of fingerprinting.
+    '''
+    x = preprocess(x, fs)
+    X = librosa.stft(x, n_fft = 4096, hop_length = 2048)
+    peaks = get_2D_peaks(librosa.amplitude_to_db(abs(X), ref=np.max))
+    
+    return generate_hashes(peaks)
+
+def preprocess(x, fs):
+        if x.ndim==2:
+            x = x[:,0].squeeze()
+        if int(fs) != int(44100):
+            x = scipy.signal.resample(x, int(x.size*44100/fs))
+        return x
+    
+def get_2D_peaks(arr2D, plot=False, amp_min=-60):
+    """
+    Extract maximum peaks from the spectogram matrix (arr2D).
+    :param arr2D: matrix representing the spectogram in dB (ref=np.max).
+    :param plot: for plotting the results.
+    :param amp_min: minimum amplitude in spectrogram in order to be considered a peak.
+    :return: a list composed by a list of frequencies and times.
+    """
+    
+    struct = generate_binary_structure(2, 2) 
+    neighborhood = iterate_structure(struct, 10) #generate a 20 by 20 matrix of the boolean, True
+    
+    local_max = maximum_filter(arr2D, footprint=neighborhood) == arr2D # find local maximums in the arr2D
+    # with in the predefined neighborhood
+    background = (arr2D == 0)
+    # apply erosion
+    eroded_background = binary_erosion(background, structure=neighborhood, border_value=1)
+    # Boolean mask of arr2D with True at peaks (applying XOR on both matrices).
+    detected_peaks = local_max != eroded_background
+    # extract peaks
+    amps = arr2D[detected_peaks]
+    freqs, times = np.where(detected_peaks)
+    # filter peaks
+    amps = amps.flatten()
+    # get indices for frequency and time
+    filter_idxs = np.where(amps > amp_min)
+    freqs_filter = freqs[filter_idxs]
+    times_filter = times[filter_idxs]
+  
+    peaks = list(zip(freqs_filter, times_filter)) #(freq, time) coordinates of all the peaks
+    
+    # plotting the constellation map/reduced spectrogram (it's not really a spectrogram
+    # since the magnitude of peaks are ignored. They are just marked using random value, 100 in this case)
+    left = np.zeros(arr2D.shape)
+    for coordinate in peaks:
+        left[coordinate] = 100
+        
+    if plot:
+        plt.figure(figsize=(15, 5))
+        librosa.display.specshow(librosa.amplitude_to_db(abs(left),ref=np.max),sr=fs,hop_length=2048,y_axis='log', x_axis='time')
+        plt.title('Constellation map')
+        plt.tight_layout()
+
+    return peaks
+
+
+def generate_hashes(peaks, fan_value=25):
+    """
+    Hash list structure:
+       sha1_hash[0:FINGERPRINT_REDUCTION]    time_offset
+        [(e05b341a9b77a51fd26, 32), ... ]
+    :param peaks: list of peak frequencies and times.
+    :param fan_value: degree to which a fingerprint can be paired with its neighbors.
+    :return: a list of hashes with their corresponding offsets.
+    """
+    hashes = []
+    for i in range(len(peaks)):
+        for j in range(1, fan_value):
+            if (i + j) < len(peaks):
+
+                freq1 = peaks[i][0]
+                freq2 = peaks[i + j][0]
+                t1 = peaks[i][1]
+                t2 = peaks[i + j][1]
+                t_delta = t2 - t1
+
+                if 0 <= t_delta <= 200:
+                    h = hashlib.sha1(f"{str(freq1)}|{str(freq2)}|{str(t_delta)}".encode('utf-8')) 
+                    #sha1 hash gives a length-40 hexdigit number. we trimmed to only 20
+                    #characters to save storage memory. This may increase the probability 
+                    #of collisions but it's a nice trade-off.
+                    hashes.append((h.hexdigest()[0:20], t1)) 
+                    # a tuple (address, offset of the anchor point)
+
+    return hashes
